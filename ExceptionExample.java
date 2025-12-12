1e1f90102d404980c748ac0745ff3c5302aee72f
sharulucky174-svg <sharulucky174@gmail.com>
2025-12-12 09:55:05 +0530

    NullPointerExceptio

--- ExceptionExample.java
+++ ExceptionExample.java
@@ -1,11 +1,10 @@
 public class ExceptionExample {
     public static void main(String[] args) {
         try {
-            int[] numbers = {1, 2, 3};
-            // Trying to access an invalid index
-            int value = numbers[5];
-            System.out.println("Value: " + value);
-        } catch (ArrayIndexOutOfBoundsException e) {
+            String text = null;  
+            int length = text.length();
+            System.out.println("Length: " + length);
+        } catch (NullPointerException e) {
             System.out.println("Caught Exception: " + e.getMessage());
         }
     }
