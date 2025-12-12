5b4d63117aaaf2f574f9c4a2df1e566194f638eb
sharulucky174-svg <sharulucky174@gmail.com>
2025-12-12 11:51:49 +0530

    String reverse

---
+++ testing.java
@@ -0,0 +1,7 @@
+public class testing {
+public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder
+        ("Hello");
+        sb.append(" World");
+        System.out.println(sb.reverse());
+}}
